105abf2b19bc94ed467905137a782843dab3c736
Derv Merkler <derv82@gmail.com>
2012-04-22 19:46:41 -0700

    Calculates up to 28 digits, then fails. Not sure why...

--- pi.py
+++ pi.py
@@ -31,102 +31,94 @@ def add_digit(digit, sign):
 		current -= digit
 	
 
+PI = '3.'
 stdout.write('3.')
 while True:
 	to_remove = []
 	# Current number (value at current decimal place)
 	current = 0
 	for i, div in enumerate(atan5):
-		# If we are at the end of the list,
-		# and the last item's decimal place is
-		# at our current position,
-		if i == len(atan5) - 1 and div.place == place:
-			#print "created atan5(%d)" % atan5step
-			atan5step += 2
-			d = DecimalDiv(16, 
-						atan5step * pow(5, atan5step),
-						subtract = not div.subtract)
-			atan5.append(d)
-		
 		if div.place == place:
+			if i == len(atan5) - 1:
+				atan5step += 2
+				d = DecimalDiv(16, 
+							atan5step * pow(5, atan5step),
+							subtract = not div.subtract)
+				atan5.append(d)
+				
 			i5 = div.next()
 			if i5 == None:
 				to_remove.append(div)
 			elif div.subtract:
 				current -= i5
-				#print "-%d (5)" % i5
 			else:
 				current += i5
-				#print "+%d (5)" % i5
-				#stdout.write("  5/%d: %d" % (div.place, i5))
-				#raw_input()
 	for remove in to_remove:
 		atan5.remove(remove)
 	
 	to_remove = []
 	for i, div in enumerate(atan239):
-		# If we are at the end of the list,
-		# and the last item's decimal place is
-		# at our current position,
-		if i == len(atan239) - 1 and div.place == place:
-			#print "created atan239(%d)" % atan239step
-			atan239step += 2
-			d = DecimalDiv(4, 
-						atan239step * pow(239, atan239step),
-						subtract = not div.subtract)
-			atan239.append(d)
-		
 		if div.place == place:
+			if i == len(atan239) - 1:
+				atan239step += 2
+				d = DecimalDiv(4, 
+							atan239step * pow(239, atan239step),
+							subtract = not div.subtract)
+				atan239.append(d)
+		
 			i239 = div.next()
 			if i239 == None:
 				to_remove.append(div)
 			elif div.subtract:
 				current -= i239
-				#print "-%d (239)" % i239
 			else:
 				current += i239
-				#print "+%d (239)" % i239
-				#stdout.write("239/%d: %d" % (div.place, i239))
-				#raw_input()
 	for remove in to_remove:
 		atan239.remove(remove)
 	
 	last10.insert(0, current)
 	for i in xrange(0, len(last10) - 1):
-		while last10[i] >= 10:
+		while last10[i] >= 5:
 			last10[i+1] += 1
 			last10[i] -= 10
 		while last10[i] < 0:
 			last10[i+1] -= 1
 			last10[i] += 10
 	
-	if len(last10) >= 11:
-		last = last10.pop(-1)
-		stdout.write(str(last))
-		#raw_input()
+	if len(last10) > 10:
+		last = str(last10.pop(-1))
+		PI += last
+		stdout.write(last)
+		stdout.flush()
 	
 	current = 0
 	place += 1
 	if place > 50:
 		break
+	
+	# Remove two decimal generators if they are both repeating
+  # the same number and their signs are opposite (add/sub)
+	# Disabled because it's useless?
+	"""
+	if place % 25:
+		for i in xrange(0, len(atan5) - 1):
+			for j in xrange(i + 1, len(atan5)):
+				if atan5[i].repeating != None and atan5[i].repeating == atan5[j].repeating and atan5[i].subtract != atan5[j].subtract:
+					atan5.pop(i)
+					i -= 1
+					atan5.pop(j)
+					j -= 2
+	"""
+
 
 print ''
+f = open('test2.txt', 'r')
+test = f.read()
+f.close()
+for i in xrange(0, len(PI)):
+	stdout.write(PI[i])
+	if PI[i] != test[i]:
+		print "\nmismatched at character %d, %s should be %s" % (i + 1, PI[i], test[i])
+		break
 exit(0)
-
-# Testing DecimalDiv
-n = 15
-d = DecimalDiv(16, n * pow(5, n))
-
-print "zeros:", d.leading_zeros
-
-x = d.next()
-count = 0
-while x != None:
-	#stdout.write(str(x))
-	#stdout.flush()
-	print x, d.place
-	x = d.next()
-	count += 1
-	if count > 15: break
-print '',
 
